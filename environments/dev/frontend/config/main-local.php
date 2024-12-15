2e5acaf9bfefb13c62cb96782f60a1460cd0455f
Andrey Murin <andrey.murin@b3it.ru>
2024-12-15 10:32:18 +0300

    доступ к отладке и gii в дев режиме

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -9,18 +9,12 @@ $config = [
     ],
 ];
 
-if (!YII_ENV_TEST) {
+if (!YII_ENV_TEST && YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => \yii\debug\Module::class,
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*'] // adjust this to your needs
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => \yii\gii\Module::class,
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*'] // adjust this to your needs
+        'allowedIPs' => ['*'] // adjust this to your needs
     ];
 }
 
